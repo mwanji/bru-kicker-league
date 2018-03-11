c7f6c19993b73b92cb973af7123e659438fda140
Mwanji Ezana <mwanji@gmail.com>
2018-03-11 16:03:00 +0100

    Handle Heroku environment

--- src/main/java/brukickerleague/Application.java
+++ src/main/java/brukickerleague/Application.java
@@ -1,7 +1,10 @@
 package brukickerleague;
 
+import spark.Spark;
+
 import javax.validation.Validation;
 import javax.validation.Validator;
+import java.util.OptionalInt;
 
 import static spark.Spark.*;
 
@@ -11,8 +14,19 @@ public class Application {
     Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
     MatchesController matchesController = new MatchesController(validator);
 
+    deploymentPort().ifPresent(Spark::port);
+
     get("/match", (req, res) -> new MatchCreationTemplate().render());
     post("/match", matchesController::createMatch);
     get("/match/:altId", (req, res) -> req.params("altId"));
   }
+
+  private static OptionalInt deploymentPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+      return OptionalInt.of(Integer.parseInt(processBuilder.environment().get("PORT")));
+    }
+    return OptionalInt.empty();
+  }
+
 }
