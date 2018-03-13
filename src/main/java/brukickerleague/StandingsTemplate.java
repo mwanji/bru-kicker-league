80da5eaed4ba8dd622de7f5ae62d11f8b4afb577
Mwanji Ezana <mwanji@gmail.com>
2018-03-13 01:03:03 +0100

    Improve match line

--- src/main/java/brukickerleague/StandingsTemplate.java
+++ src/main/java/brukickerleague/StandingsTemplate.java
@@ -15,14 +15,13 @@ public class StandingsTemplate {
     return new Page("Standings",
       h1("Standings"),
       each(matches, match -> div(attrs(".mb-3"),
-        a(attrs(".btn.btn-dark"),
+        a(attrs(".btn.btn-dark.mr-4"),
           span(match.getTeam1FullName()),
-          span(attrs(".badge.badge-light"), Integer.toString(match.getTeam1Score()))
+          span(attrs(".badge.badge-light.ml-2"), Integer.toString(match.getTeam1Score()))
         ).withHref("/match/" + match.getAltId()),
-        span(" - "),
         a(attrs(".btn.btn-light"),
           span(match.getTeam2FullName()),
-          span(attrs(".badge.badge-dark"), Integer.toString(match.getTeam2Score()))
+          span(attrs(".badge.badge-dark.ml-2"), Integer.toString(match.getTeam2Score()))
         ).withHref("/match/" + match.getAltId())
       ))
     ).render();
