52061db68cdbfd7c2a14b1e708cbb6e7c2988f1b
Moandji Ezana <mwanji@gmail.com>
2018-03-13 09:14:01 +0100

    Make button group span entire container

--- src/main/java/brukickerleague/StandingsTemplate.java
+++ src/main/java/brukickerleague/StandingsTemplate.java
@@ -16,12 +16,12 @@ public class StandingsTemplate {
       h1(attrs(".display-1.mb-4"), "Standings"),
       each(matches, match -> div(attrs(".mb-3"),
         div(attrs(".btn-group.w-100"),
-          a(attrs(".btn.btn-dark"),
+          a(attrs(".btn.btn-dark.w-50"),
             iff(!match.hasEnded(), span(attrs(".badge.badge-danger.mr-3"), "LIVE")),
             span(match.getTeam1FullName()),
             span(attrs(".badge.badge-light.ml-2"), Integer.toString(match.getTeam1Score()))
           ).withHref("/match/" + match.getAltId()),
-          a(attrs(".btn.btn-light"),
+          a(attrs(".btn.btn-light.w-50"),
             span(match.getTeam2FullName()),
             span(attrs(".badge.badge-dark.ml-2"), Integer.toString(match.getTeam2Score()))
           ).withHref("/match/" + match.getAltId())
