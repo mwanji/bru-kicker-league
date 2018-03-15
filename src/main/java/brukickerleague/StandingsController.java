6a62880cabea1c3351159cd24bbeae3d109fe38c
Moandji Ezana <mwanji@gmail.com>
2018-03-15 12:15:25 +0100

    Add missing collect

--- src/main/java/brukickerleague/StandingsController.java
+++ src/main/java/brukickerleague/StandingsController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import spark.Request;
 import spark.Response;
 
+import java.util.stream.Collectors;
 import java.util.List;
 
 @AllArgsConstructor
@@ -12,7 +13,7 @@ public class StandingsController {
   private final Db db;
 
   public Object getStandings(Request req, Response res) {
-    List<Match> liveMatches = db.inTx(tx -> tx.all(Match.class, "createdAt").stream().filter(match -> !match.hasEnded()));
+    List<Match> liveMatches = db.inTx(tx -> tx.all(Match.class, "createdAt").stream().filter(match -> !match.hasEnded()).collect(Collectors.toList());
     List<Match> fortnightlyMatches = db.inTx(tx -> tx.query(Match.class, "Match.ended"));
     Standings standings = new Standings(fortnightlyMatches);
 
